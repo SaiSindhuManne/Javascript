d3ee95d6af98a36202eff39a251aead6a4dcc028
Sai Sindhu Manne <108350175+SaiSindhuManne@users.noreply.github.com>
2024-02-26 17:41:46 +0000

    Data types and ECMA script

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,9 @@
-console.log("Hey!")
+const accountId = 12345
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+console.table([accountEmail,accountId,accountPassword]);
+
+// "use strict"; treat all java code as newer version
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
